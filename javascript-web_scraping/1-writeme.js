34396e2acf84a7f8d7b3f303c9c2b93e0fbb095b
lgallogomez <l.gallogomez@gmail.com>
2024-02-05 21:11:55 -0500

    updated 1st file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -6,7 +6,7 @@ const filename = process.argv[2];
 
 const toWrite = process.argv[3];
 
-fs.writeFile(filename, toWrite, (err, data) => {
+fs.writeFile(filename, toWrite, (err) => {
   if (err) {
     console.error(err);
   }
